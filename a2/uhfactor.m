cebb2251a3af9299576349d4d9b619f876b7ae20
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-02-16 05:36:12 -0500

    q2:a complete REVIEW IT AFTERWARDS

--- a2/uhfactor.m
+++ a2/uhfactor.m
@@ -1,21 +1,37 @@
-function [L, A, piv] = uhfactor(A)
+function [LU, piv] = uhfactor(A)
 %UHFACTOR Determine a LU factorization of an upper Hessenberg matrix
 % using Gaussian elimination with row pivotings
   n = length(A);
   piv = 1:n;
-  L = zeros(n);
+  LU = zeros(n);
   I = eye(n);
   
   for i = 1:n-1
+    % Row Pivoting:
+    %   The only non-zero value in column is A(i+1, i)
+    if A(piv(i+1), i) > A(piv(i), i)
+      piv([i i+1]) = piv([i+1 i]);
+      
+      % Apply same permutation on L
+      LU([i i+1], :) = LU([i+1 i], :);
+    end
+    
     for j = i+1:n
-      L(j, i) = A(piv(j), i) / A(piv(i), i);
+      LU(j, i) = A(piv(j), i) / A(piv(i), i);
     end
     e = zeros(1, n);
     e(i) = 1;
-    m = L(:, i);
-    A = (I - m*e) * A;
+    m = LU(:, i);
+    M = I - m*e;
+    A(piv, :) = M * A(piv, :);
   end
   
-  L = L + I;
+  LU = LU + I;
+  A = A(piv, :);
+  
+  % Combine L and U forms
+  for i = 1:n
+    LU(i, i:n) = A(i, i:n);
+  end
 end
 
