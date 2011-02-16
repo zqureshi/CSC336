d58ee3f6dab04fcfc19e2f92cbb4a863274c7446
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-02-16 08:33:29 -0500

    rewrote q2a

--- a2/uhfactor.m
+++ a2/uhfactor.m
@@ -1,37 +1,42 @@
-function [LU, piv] = uhfactor(A)
-%UHFACTOR Determine a LU factorization of an upper Hessenberg matrix
-% using Gaussian elimination with row pivotings
-  n = length(A);
-  piv = 1:n;
-  LU = zeros(n);
-  I = eye(n);
-  
-  for i = 1:n-1
-    % Row Pivoting:
-    %   The only non-zero value in column is A(i+1, i)
-    if A(piv(i+1), i) > A(piv(i), i)
-      piv([i i+1]) = piv([i+1 i]);
-      
-      % Apply same permutation on L
-      LU([i i+1], :) = LU([i+1 i], :);
-    end
-    
-    for j = i+1:n
-      LU(j, i) = A(piv(j), i) / A(piv(i), i);
-    end
-    e = zeros(1, n);
-    e(i) = 1;
-    m = LU(:, i);
-    M = I - m*e;
-    A(piv, :) = M * A(piv, :);
+function [L, A, piv] = uhfactor(A)
+%UHFACTOR LU factorization of A with row pivoting
+
+% Assuming A is n x n
+n = length(A);
+L = zeros(n);
+piv = 1:n;
+I = eye(n);
+
+for i = 1:n-1
+  % Partial Pivoting
+  if A(piv(i+1), i) > A(piv(i), i)
+    piv([i i+1]) = piv([i+1 i]);
+    L([i i+1], :) = L([i+1 i], :);
   end
   
-  LU = LU + I;
-  A = A(piv, :);
-  
-  % Combine L and U forms
-  for i = 1:n
-    LU(i, i:n) = A(i, i:n);
+  for j = i+1:n
+    L(j, i) = A(piv(j), i) / A(piv(i), i);
   end
+  e = zeros(1, n);
+  e(i) = 1;
+  m = L(:, i);
+  M = I - m*e;
+  
+  % Matrix multiplication with pivoting
+  T = A;
+  for x = 1:n
+    for y = 1:n
+      sum = 0;
+      for z = 1:n
+        sum = sum + M(x, z) * A(piv(z), y);
+      end
+      T(piv(x), y) = sum;
+    end
+  end
+  A=T;
+end
+
+L = L + I;
+A = A(piv, :);
 end
 
