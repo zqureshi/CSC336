64eade496c26f8078d18e123b92e460c7eedb0d2
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-02-15 23:09:51 -0500

    LU factorization WITHOUT pivoting works

--- a2/uhfactor.m
+++ a2/uhfactor.m
@@ -1,7 +1,21 @@
-function LU = uhfactor(A)
+function [L, A, piv] = uhfactor(A)
 %UHFACTOR Determine a LU factorization of an upper Hessenberg matrix
 % using Gaussian elimination with row pivotings
-
-
+  n = length(A);
+  piv = 1:n;
+  L = zeros(n);
+  I = eye(n);
+  
+  for i = 1:n-1
+    for j = i+1:n
+      L(j, i) = A(piv(j), i) / A(piv(i), i);
+    end
+    e = zeros(1, n);
+    e(i) = 1;
+    m = L(:, i);
+    A = (I - m*e) * A;
+  end
+  
+  L = L + I;
 end
 
